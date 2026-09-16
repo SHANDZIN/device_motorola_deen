4abb235f43659f214b6621041ab932227957c156
Vhmit <viktor.9630@protonmail.com>
2026-09-16 20:37:18 -0300

    deen: ignore neverallows de bosta cu mijo

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -204,6 +204,7 @@ VENDOR_SECURITY_PATCH := 2021-10-01
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
 BOARD_VNDK_VERSION := current
